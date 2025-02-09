4560454e1f28a6cdae258fb0f381d94b5733747b
Marcos <devmarcos7@gmail.com>
2025-02-09 15:53:24 -0300

    minsize e maxsize

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import customtkinter as ctk
 
 window = ctk.CTk()
 window.title('Login')
-window.geometry('400x400')
+window.geometry('800x600')
+window.maxsize(width=900, height=700)
+window.minsize(width=800, height=600)
 
 
 window.mainloop()
