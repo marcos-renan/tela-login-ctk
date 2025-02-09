99df65699f4fed60588cbc7e4c1250d24d4b3950
Marcos <devmarcos7@gmail.com>
2025-02-09 16:04:20 -0300

    documentando

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import customtkinter as ctk
+import customtkinter as ctk #importando o CTk
 
-window = ctk.CTk()
-window.title('Login')
-window.geometry('800x600')
-window.maxsize(width=900, height=700)
-window.minsize(width=800, height=600)
+window = ctk.CTk() #definindo janela
+window.title('Login') #titulo da janela
+window.geometry('800x600') #definindo tamanho da janela
+window.maxsize(width=900, height=700) #tamanho maximo
+window.minsize(width=800, height=600) #tamanho minimo
+ctk.set_appearance_mode('dark') #tema da janela
 
 
-window.mainloop()
+window.mainloop() #loop que ira iniciar a janela
